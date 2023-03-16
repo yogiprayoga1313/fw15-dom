6e2bed3001b2da7115b7437ce7448737c8cf8c2b
Yogi Prayoga <yogiprayoga1333@gmail.com>
2023-03-16 16:42:59 +0700

    Update, Tugas week 2 Soal No 2

---
+++ TugasWeek2No2.js
@@ -0,0 +1,28 @@
+const Biodata = {
+    name: 'yogi',
+    age: 25,
+    hobbies: ['Badminton', 'Voly'],
+    isMarried: false,
+    schoolList: [
+        {
+            name: 'Smk Boedi Oetomo Cilacap',
+            yearIn: 2012,
+            yearOut: 2015,
+            major: null,
+        },
+    ],
+    skill: [
+        {
+            skillName: 'Html, Css, JS',
+            level:'beginner',
+        },
+    ],
+    interestInCoding: true
+};
+console.log(Biodata);
+
+
+
+
+
+ 
